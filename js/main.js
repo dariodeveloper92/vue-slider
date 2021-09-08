cb1612a1515ecfb92a9fc6eff9f7f81a4589ac2c
Dario <dario.defrancisci92@gmail.com>
2021-09-08 14:17:37 +0200

    creato scaffolding

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Vue js
+
+Vue.config.devtools = true; // scrivere per attivare l'estenzione sul browser
+
+//Consegna:
+//Rifare l'esercizio dello slider come fatto assieme in classe.
+
+
+//Bonus:
+//Creare il navigatore in basso con i pallini bianchi (blu se corrispondente all'immagine attualmente visualizzata)
+//Applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente.

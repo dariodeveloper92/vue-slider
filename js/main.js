acfcac65c760c9c09bb9c3a1b758cd910e204dda
Dario <dario.defrancisci92@gmail.com>
2021-09-08 16:30:31 +0200

    Parte logica caricata

--- js/main.js
+++ js/main.js
@@ -5,6 +5,29 @@ Vue.config.devtools = true; // scrivere per attivare l'estenzione sul browser
 //Consegna:
 //Rifare l'esercizio dello slider come fatto assieme in classe.
 
+const app = new Vue(
+    {
+        el: '#root',
+        data: {
+            counterPhoto: 0,
+            photos: [
+                'img/image1.jpg',
+                'img/image2.jpg',
+                'img/image3.jpg',
+                'img/image4.jpg',
+            ]
+        },
+        methods: {
+            prevPhoto() {   // Foto precedente
+                this.counterPhoto -= 1;
+                if(this.counterPhoto < 0) this.counterPhoto = (this.photos.lenght - 1);
+            },
+            nextPhoto() {   // Foto successiva
+                this.counterPhoto += 1;
+                if(this.counterPhoto == (this.photos.lenght)) this.counterPhoto - 0;
+            }
+        }
+})
 
 //Bonus:
 //Creare il navigatore in basso con i pallini bianchi (blu se corrispondente all'immagine attualmente visualizzata)
